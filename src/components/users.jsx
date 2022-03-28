83cd70de3370db8e6b7ab756b1528a18eb330e15
Nikit099 <nechit1337@gmail.com>
2022-03-28 13:11:44 +0300

    add more text

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -22,7 +22,7 @@ const Users = () => {
                 <th scope="col">Профессия</th>
                 <th scope="col">Встретился, раз</th>
                 <th scope="col">Оценка</th>
-                <th scope="col"></th>
+                <th scope="col">Избранное</th>
                 <th scope="col"></th>
                 </tr>
             </thead>
