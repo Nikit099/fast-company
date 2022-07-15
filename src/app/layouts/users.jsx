c0f5b816f96e23573ac28b7b17cb865c1d32a6a4
Nikit099 <nechit1337@gmail.com>
2022-07-15 19:49:39 +0300

    made http.servise

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import EditPage from "../components/page/editPage";
+import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
     return (
         <>
+        <UserProvider>
             {userId ? (
                 edit ? (
                     <EditPage />
@@ -17,6 +19,7 @@ const Users = () => {
             ) : (
                 <UsersListPage />
             )}
+            </UserProvider>
         </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://7e26cfa133754e0d9d42cdb2a6edd105@o1306530.ingest.sentry.io/6549248",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+});
 
 ReactDOM.render(
     <React.StrictMode>
